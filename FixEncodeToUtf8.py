21de595901d458b3a0db6e6c4d89260e735f37ab
taoyy <tyyphp@163.com>
2022-04-21 15:12:41 +0800

    add FixEncodeToUtf8

---
+++ FixEncodeToUtf8.py
@@ -0,0 +1,17 @@
+import os
+from chardet import detect
+fileSuffix = 'txt'
+fns = []
+filedir = os.path.join(os.path.abspath('.'), "")
+file_name = os.listdir(os.path.join(os.path.abspath('.'), ""))
+for fn in file_name:
+    if fn.endswith(fileSuffix):
+        fns.append(os.path.join(filedir, fn))
+for fn in fns:
+    with open(fn, 'rb+') as fp:
+        content = fp.read()
+        codeType = detect(content)['encoding']
+        content = content.decode(codeType, "ignore").encode("utf8")
+        fp.seek(0)
+        fp.write(content)
+        print(fn, "：已修改为utf8编码")
